51d88376c3dc87658aed1a3167c92e03fb7d694c
Ralph Schaer <ralphschaer@gmail.com>
2023-11-11 11:19:28 +0100

    Add example with GPT-4 Vision

---
+++ src/main/java/ch/rasc/openai4j/example/ChatCompletionsVisionExample.java
@@ -0,0 +1,30 @@
+package ch.rasc.openai4j.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.rasc.openai4j.OpenAIClient;
+import ch.rasc.openai4j.chatcompletions.SystemMessage;
+import ch.rasc.openai4j.chatcompletions.UserMessage;
+import ch.rasc.openai4j.chatcompletions.UserMessage.Content;
+import ch.rasc.openai4j.chatcompletions.UserMessage.ImageContent;
+import ch.rasc.openai4j.chatcompletions.UserMessage.TextContent;
+
+public class ChatCompletionsVisionExample {
+	public static void main(String[] args) {
+		String apiKey = Util.getApiKey();
+		var client = OpenAIClient.create(c -> c.apiKey(apiKey));
+
+		List<Content> content = new ArrayList<>();
+		content.add(TextContent.of("What’s in this image?"));
+		content.add(ImageContent.of(
+				"https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"));
+
+		var response = client.chatCompletions
+				.create(r -> r.addMessage(SystemMessage.of("You are a helpful assistant"))
+						.addMessage(UserMessage.of(content)).model("gpt-4-vision-preview")
+						.maxTokens(300));
+		System.out.println(response.choices()[0].message().content());
+
+	}
+}
