2b7a8164c357f53ff94962a69cf4617dd5c3c8f5
Ralph Schaer <ralphschaer@gmail.com>
2023-11-18 11:47:27 +0100

    Update example

--- src/main/java/ch/rasc/openai4j/example/EmbeddingsExample.java
+++ src/main/java/ch/rasc/openai4j/example/EmbeddingsExample.java
@@ -1,6 +1,7 @@
 package ch.rasc.openai4j.example;
 
 import java.util.Arrays;
+import java.util.List;
 
 import ch.rasc.openai4j.OpenAIClient;
 import ch.rasc.openai4j.embeddings.EmbeddingCreateRequest;
@@ -12,16 +13,39 @@ public class EmbeddingsExample {
 		var client = OpenAIClient.create(c -> c.apiKey(apiKey));
 
 		EmbeddingCreateRequest request = EmbeddingCreateRequest.builder()
-				.addInput("HelloWorld").addInput("HelloWorld2")
+				.input(List.of("HelloWorld", "HelloWorld2"))
 				.encodingFormat(EncodingFormat.BASE64).model("text-embedding-ada-002")
 				.build();
 		var response = client.embeddings.create(request);
 		System.out.println(response.data().get(0).embedding().base64());
 
-		response = client.embeddings.create(r -> r.addInput("HelloWorld")
-				.addInput("HelloWorld2").encodingFormat(EncodingFormat.FLOAT)
-				.model("text-embedding-ada-002"));
+		request = EmbeddingCreateRequest.builder()
+				.input(List.of("HelloWorld", "iPhone", "Monkey"))
+				.model("text-embedding-ada-002").build();
+		var response2 = client.embeddings.create(request);
+		for (var embedding : response2.data()) {
+			System.out.println(Arrays.toString(embedding.embedding().doubleArray()));
+		}
+
+		response = client.embeddings.create(r -> r.input("HelloWorld")
+				.encodingFormat(EncodingFormat.FLOAT).model("text-embedding-ada-002"));
 		double[] embeddings = response.data().get(0).embedding().doubleArray();
 		System.out.println(Arrays.toString(embeddings));
+		System.out.println(Arrays.equals(embeddings,
+				response2.data().get(0).embedding().doubleArray()));
+
+		response = client.embeddings.create(r -> r.input("iPhone")
+				.encodingFormat(EncodingFormat.FLOAT).model("text-embedding-ada-002"));
+		embeddings = response.data().get(0).embedding().doubleArray();
+		System.out.println(Arrays.toString(embeddings));
+		System.out.println(Arrays.equals(embeddings,
+				response2.data().get(1).embedding().doubleArray()));
+		
+		response = client.embeddings.create(r -> r.input("Monkey")
+				.encodingFormat(EncodingFormat.FLOAT).model("text-embedding-ada-002"));
+		embeddings = response.data().get(0).embedding().doubleArray();
+		System.out.println(Arrays.toString(embeddings));
+		System.out.println(Arrays.equals(embeddings,
+				response2.data().get(2).embedding().doubleArray()));		
 	}
 }
