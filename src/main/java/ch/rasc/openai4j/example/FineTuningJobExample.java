5332c5ec48ebbd949580cb48415014d6ac727ff7
Ralph Schaer <ralphschaer@gmail.com>
2023-11-19 16:02:18 +0100

    Update

--- src/main/java/ch/rasc/openai4j/example/FineTuningJobExample.java
+++ src/main/java/ch/rasc/openai4j/example/FineTuningJobExample.java
@@ -1,12 +1,23 @@
 package ch.rasc.openai4j.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import ch.rasc.openai4j.OpenAIClient;
+import ch.rasc.openai4j.finetuningjobs.FineTuningJobCreateRequest;
 
 public class FineTuningJobExample {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonProcessingException {
 		String apiKey = Util.getApiKey();
 		var client = OpenAIClient.create(c -> c.apiKey(apiKey));
 
+		ObjectMapper om = new ObjectMapper();
+		var c = FineTuningJobCreateRequest.builder().model("test").validationFile("val")
+				.trainingFile("train")
+				.hyperparameters(
+						h -> h.batchSize(1).learningRateMultiplier(2).nEpochs(3))
+				.build();
+		System.out.println(om.writeValueAsString(c));
 		var jobs = client.fineTuningJobs.list();
 		for (var job : jobs.data()) {
 			System.out.println(job);
